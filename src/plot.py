38f77af844a01ca4dda51852ab50f18b6c7def93
ebylmz <ebylmz17@gmail.com>
2023-08-18 10:21:04 +0200

    add informative messages

--- src/plot.py
+++ src/plot.py
@@ -6,33 +6,41 @@ import csv
 import sys
 
 def plot_tsne(sentences, out_file):
-    # Get sentence embeddings
+    print("Generating sentence embeddings...")
     model = SentenceTransformer('distilbert-base-nli-mean-tokens')
     embeddings = model.encode(sentences)
-    # Compute t-SNE embeddings
+    
+    print("Performing t-SNE dimensionality reduction...")
     tsne_embeddings = TSNE(n_components=2, random_state=42).fit_transform(embeddings)
-    # Create an interactive plot
+    
+    print("Creating t-SNE plot...")
     df = pd.DataFrame(tsne_embeddings, columns=['x', 'y'])
     df['sentence'] = sentences
     fig = px.scatter(df, x='x', y='y', hover_data=['sentence'])
+    
+    print("Saving t-SNE plot as an image...")
     fig.write_image(out_file)
+    print(f"Plot saved as {out_file}")
 
 def load_indications(filename):
+    print(f"Loading indication data from '{filename}'...")
     data = []
     with open(filename, 'r') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
             data.append(row['text'])
+    print(f"Loaded {len(data)} indications")
     return data
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        sys.exit(f"Right usage: {sys.argv[0]} [in: dailymed.csv] [plot.png]") 
+        sys.exit(f"Usage: {sys.argv[0]} [input_csv_file] [output_image_file]") 
 
-    print("Loading indication data from CSV file...")
-    ind = load_indications(sys.argv[1])
+    input_csv = sys.argv[1]
+    output_image = sys.argv[2]
+
+    indications = load_indications(input_csv)
     
-    print("Generating t-SNE plot...")
-    plot_tsne(ind, sys.argv[2])
+    plot_tsne(indications, output_image)
 
-    print("t-SNE plot generated and saved.")
+    print("Completed")
