6b1ce361bda83899dc94d8fa04d76132b960fe41
ebylmz <ebylmz17@gmail.com>
2023-08-18 14:44:26 +0200

    clean indication full text

--- src/clean.py
+++ src/clean.py
@@ -4,29 +4,30 @@ import random
 import pandas as pd
 
 def clean_text(text):
-    regex_pattern = r'(\s){2,}\n'
-    text = re.sub(regex_pattern, '\n', text).strip()
-    
-    # Remove non-ASCII characters
-    text = ''.join([char if ord(char) < 128 else '' for char in text])
-
-    return text
+    cleaned_text = re.sub(r'\n+', '\n', text)  # Remove extra line breaks
+    cleaned_text = re.sub(r'\n\s+', '\n', cleaned_text)  # Remove leading spaces after line breaks
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text)  # Replace multiple spaces with a single space
+    cleaned_text = re.sub(r'^\d* ','', cleaned_text)
+    cleaned_text = re.sub(r'\d* INDICATIONS AND USAGE','', cleaned_text)
+    cleaned_text = re.sub(r'INDICATIONS AND USAGE','', cleaned_text)
+    cleaned_text = cleaned_text.encode("ascii", "ignore").decode()
+    return cleaned_text
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        sys.exit(f"Right usage: {sys.argv[0]} [in: dailymed.csv] [in: min-len] [in: max-len] [out: dailymed_clean.csv]") 
+    if len(sys.argv) != 3:
+        sys.exit(f"Right usage: {sys.argv[0]} [input_csv_file] [output_csv_file]") 
         
     in_file = sys.argv[1]
-    min_len = int(sys.argv[2])
-    max_len = int(sys.argv[3])
-    out_file = sys.argv[4]
+    out_file = sys.argv[2]
     
     # Read the CSV file into a DataFrame
     df = pd.read_csv(in_file)
 
     # Apply the clean_text function and filter rows by text length in one step
     df['text'] = df['text'].apply(clean_text)
-    df = df[df['text'].str.len().between(min_len, max_len)]
+    # df = df[df['text'].str.len().between(min_len, max_len)]
+
+    df.drop_duplicates(subset=['text'])
 
     # Shuffle the order of filtered rows randomly (set seed for reproducibility)
     random.seed(42) 
