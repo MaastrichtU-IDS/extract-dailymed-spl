f125de7201745da2d66a3a9e321c73f49c1921c5
ebylmz <ebylmz17@gmail.com>
2023-08-22 17:07:23 +0200

    add final strip

--- src/clean.py
+++ src/clean.py
@@ -1,37 +1,32 @@
-import sys
+import argparse
 import re
 import random
 import pandas as pd
 
 def clean_text(text):
-    cleaned_text = re.sub(r'\n+', '\n', text)  # Remove extra line breaks
-    cleaned_text = re.sub(r'\n\s+', '\n', cleaned_text)  # Remove leading spaces after line breaks
-    cleaned_text = re.sub(r'\s+', ' ', cleaned_text)  # Replace multiple spaces with a single space
+    cleaned_text = re.sub(r'\n+', '\n', text)
+    cleaned_text = re.sub(r'\n\s+', '\n', cleaned_text)
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
     cleaned_text = re.sub(r'^\d* ','', cleaned_text)
     cleaned_text = re.sub(r'\d* INDICATIONS AND USAGE','', cleaned_text)
-    cleaned_text = re.sub(r'INDICATIONS AND USAGE','', cleaned_text)
     cleaned_text = cleaned_text.encode("ascii", "ignore").decode()
-    return cleaned_text
+    return cleaned_text.strip()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit(f"Right usage: {sys.argv[0]} [input_csv_file] [output_csv_file]") 
-        
-    in_file = sys.argv[1]
-    out_file = sys.argv[2]
+    parser = argparse.ArgumentParser(description="Clean and shuffle text data from a CSV file.")
+    parser.add_argument("input_file", help="Path to the input CSV file")
+    parser.add_argument("output_file", help="Path to the output CSV file")
+    args = parser.parse_args()
+
+    in_file = args.input_file
+    out_file = args.output_file
     
-    # Read the CSV file into a DataFrame
     df = pd.read_csv(in_file)
 
-    # Apply the clean_text function and filter rows by text length in one step
     df['text'] = df['text'].apply(clean_text)
-    # df = df[df['text'].str.len().between(min_len, max_len)]
+    df = df.drop_duplicates(subset=['text'])
 
-    df.drop_duplicates(subset=['text'])
-
-    # Shuffle the order of filtered rows randomly (set seed for reproducibility)
-    random.seed(42) 
+    random.seed(42)
     df_shuffled = df.sample(frac=1).reset_index(drop=True)
 
-    # Save the shuffled and cleaned DataFrame to a CSV file
     df_shuffled.to_csv(out_file, index=False)
